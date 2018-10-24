931f21592e1d20e3afc8442dfa2f1beca8f3bfb8
Mike Smith <mikerhyssmith@googlemail.com>
2018-10-24 22:36:06 +0100

    add command line arguments for generating from hex

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,25 +1,52 @@
 import {Command, flags} from '@oclif/command'
+// @ts-ignore
+import hexToHsl from 'hex-to-hsl'
+//@ts-ignore
+import * as generate from 'lyft-coloralgorithm'
+
+// steps - number of steps to generate
+// lum_curve
+// sat_curve
+// hue_curve
+// lum_start
+// lum_end
+// sat_start
+// sat_end
+// sat_rate
+// hue_start - Colour
+// hue_end - Colour
+// modifier
 
 export default class Generate extends Command {
   static description = 'generate a series of colour steps'
 
   static flags = {
-    hex: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    start: flags.string({description: 'start hex value'}),
+    end: flags.string({description: 'end hex value'}),
+    steps: flags.integer({char: 's', description: 'number of colours to generate'}),
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(Generate)
+    const {flags} = this.parse(Generate)
+    const startHSL = hexToHsl(flags.start)
+    const endHSL = hexToHsl(flags.end)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/mikesmith/Documents/Repositories/colorbox/src/commands/generate.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const algorithmArguments = {
+      steps: flags.steps,
+      lum_start: startHSL[2],
+      lum_end: endHSL[2],
+      sat_start: startHSL[1],
+      sat_end: endHSL[1],
+      hue_start: startHSL[0],
+      hue_end: endHSL[0],
+      hue_curve: 'easeInQuad',
+      lum_curve: 'easeOutQuad',
+      sat_curve: 'easeOutQuad',
+      modifier: 10,
+      graph: 'sat',
     }
+
+    const colourSequence = generate({specs: algorithmArguments})
+    this.log(colourSequence)
   }
 }
